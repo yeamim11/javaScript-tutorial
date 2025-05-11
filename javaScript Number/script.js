0c7b39f910b11feed8be49d863e8a9f59e685697
yeamim11 <abubokorsiddikyeamim7809@gmail.com>
2025-05-12 01:14:39 +0600

    add

---
+++ javaScript Number/script.js
@@ -0,0 +1,21 @@
+let a = 2;
+let b = 4; 
+let value ="";
+
+
+// while (a != Infinity) {
+//     a = a*a;
+//     value = value + a + '<br>';
+// }
+// document.getElementById('title').innerHTML = value;
+
+
+
+// while (b <= 100) {
+//     if (b % 2 == 0) {
+//         value += b + " ";
+//     }
+//     a++;
+// }
+// document.getElementById('title').innerHTML = value;
+
