f84681806fcc38c28817f0913493674ec3da1453
yeamim11 <abubokorsiddikyeamim7809@gmail.com>
2025-05-13 01:02:02 +0600

    add

---
+++ JavaScript Date/script.js
@@ -0,0 +1,14 @@
+const myDate = new Date();
+console.log( typeof myDate);
+console.log(myDate);
+console.log(myDate.toDateString());
+console.log(myDate.toTimeString()); 
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleDateString());   
+
+
+let upDate = new Date("2000-12-13");
+let upDates = new Date("2030-12-13");
+
+console.log(upDates);
+console.log(upDate.toDateString());
